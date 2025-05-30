3f61862904e829f304258ba16b90d4ded13014d4
gmii <gumilarharisubagja28@gmail.com>
2025-05-30 19:53:54 +0700

    Refactor task management with modal forms
    Updated `addTaskButton_Click` and `editTaskButton_Click` methods in `Dashboard.cs` to utilize new modal forms (`AddTask` and `EditTask`) for task addition and editing. Introduced `AddTask` and `EditTask` classes in `AddTask.cs` and `EditTask.cs`, respectively, to encapsulate task management logic and UI. Created corresponding designer files for UI components and added resource files for localization support. These changes enhance user interaction and improve the overall structure and maintainability of the task management feature.

---
+++ GUI/EditTask.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ModelTask = API.Model.Task;
+
+namespace GUI
+{
+    public partial class EditTask : Form
+    {
+        private readonly string _username;
+        private readonly ModelTask _task;
+
+        public EditTask(string username, ModelTask task)
+        {
+            InitializeComponent();
+            _username = username;
+            _task = task;
+        }
+    }
+}
